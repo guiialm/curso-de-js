6cf94ba20e17aae802a8852b71ffcf1e4dbd08e1
guilherme <guguiejo@gmail.com>
2023-05-25 14:57:54 -0300

    exercicios de repetiçao

---
+++ aula013/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+do {
+    console.log(` Passo ${c}`)
+    c++
+} while(c <= 2)
+
+
+
+
+
+/*var c = 1
+while (c < 6 ) {
+    console.log(`Passo ${c} `)
+    c++  // é igual c = c + 1
+}*/
+
+
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/
+
+
