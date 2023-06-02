b84e3b181ba0f15327dc2d61b44dc8dee63d906c
guilherme <guguiejo@gmail.com>
2023-06-02 10:47:21 -0300

    tentativa

--- aula016/script.js
+++ aula016/script.js
@@ -2,7 +2,7 @@ function parimp() {
     var n = document.getElementById('txtn')
     var res = document.getElementById('res')
 
-    if (n%2 == 0) {
+    if (n % 2 == 0) {
         res.innerHTML = ` O numero ${n} é par `
     } else {
         res.innerHTML = ` O numer ${n} é impar `
