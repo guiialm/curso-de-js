deeba4ede81c8b7f33d61f282a578d922def1245
guilherme <guguiejo@gmail.com>
2023-06-05 11:25:24 -0300

    tentativa ex 1

---
+++ aula018/script.js
@@ -0,0 +1,30 @@
+let num = document.getElementById('fnum')
+let lista = document.getElementById('flista')
+let res = document.getElementById('res')
+let valores = []
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, lista) {
+    if (lista.indexOf(Number(n))!= -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        window.alert (' tudo ok')
+    } else {
+        window.alert ('Valor inválido ou ja encontrado na lista')
+    }
+} 
+
